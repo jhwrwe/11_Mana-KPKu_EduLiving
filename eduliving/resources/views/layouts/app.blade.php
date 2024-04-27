1e97e7bff2453549a2e087e19eab849a3d301b41
xAnonym101 <xvgonst@gmail.com>
2024-04-27 21:29:02 +0700

    21:28

--- eduliving/resources/views/layouts/app.blade.php
+++ eduliving/resources/views/layouts/app.blade.php
@@ -221,7 +221,7 @@
                                     href="\">Home</a>
                             </li>
                             @if (Route::has('login'))
-<li class="nav-item">
+                            <li class="nav-item">
                                     <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @endif
@@ -251,29 +251,29 @@
                                     </form>
                                 </div>
                             </li>
-    @if (Auth::user()->isUser())
-        <li class="nav-item">
-            <a class="nav-link" href="\Find">Find</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="\Animals">Animals</a>
-        </li>
-    @endif
-    @if (Auth::user()->isAdmin())
-    <li class="nav-item">
-        <a class="nav-link" href="/adminhome">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/animalsadmin">animals</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/adminuser">All user</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/adminanimals">All Animal</a>
-    </li>
-@endif
-@endauth
+                            @if (Auth::user()->isUser())
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="\Find">Find</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="\Animals">Animals</a>
+                                </li>
+                            @endif
+                            @if (Auth::user()->isAdmin())
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="/adminhome">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="/animalsadmin">animals</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="/adminuser">All user</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="/adminanimals">All Animal</a>
+                                </li>
+                            @endif
+                        @endauth
 
                     </ul>
 
