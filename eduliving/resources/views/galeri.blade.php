1825cdcd207869ba3f30c498b479ec0066c07a22
xAnonym101 <xvgonst@gmail.com>
2024-04-27 23:45:05 +0700

    23:45

--- eduliving/resources/views/galeri.blade.php
+++ eduliving/resources/views/galeri.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 @foreach($animals as $animal)
                 <a href="{{ route('detail', ['id' => $animal->id]) }}" class="col-lg-3 col-md-4 col-6 my-3">
-                    <img src="{{ $animal->image }}" class="img-fluid card">
+                    <img src="/images/{{ $animal->image }}" class="img-fluid card">
                 </a>
                 @endforeach
             </div>
